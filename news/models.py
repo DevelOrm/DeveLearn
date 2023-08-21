b06e9953a7f3940819e9ddbc02251f85176b3e74
LeeJiSeop <jjiisub@naver.com>
2023-08-21 16:58:31 +0900

    🐛 fix: News 모델 url 길이 제한

--- news/models.py
+++ news/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=100)
-    link = models.URLField()
+    title = models.TextField(max_length=255)
+    link = models.URLField(max_length=2048)
     written_at = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
