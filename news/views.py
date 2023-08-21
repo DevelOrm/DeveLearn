de2701a26af39f803d1fa50db2716eaef2feefac
LeeJiSeop <jjiisub@naver.com>
2023-08-21 12:19:01 +0900

    ✨ feat: NewsBotView 추가

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class NewsBotView(APIView):
+    def post(self, request, format=None):
+        data = request.data
+        print(data['33'])
+        return Response({'message': 'JSON data received successfully'}, status=status.HTTP_201_CREATED)
