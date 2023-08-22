57e7165eda2f7bd6b79d71233744337da7e935bb
주기현 <0318joo@naver.com>
2023-08-22 09:31:30 +0900

    ✨ feat: 게시판 기능 생성

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import ClassroomView
+from .views import ClassroomView, ClassroomDetailView, ClassroomTagView, TestBoardView, TestBoardDetailView, \
+    TestBoardByClassView, TestView, TestDetailView, TestByBoardView, LectureNoteBoardView, LectureNoteBoardDetailView, \
+    LectureNoteBoardByClassView, LectureNoteView, LectureNoteDetailView, LectureNoteByBoardView, QuestionBoardView, \
+    QuestionBoardDetailView, QuestionBoardByClassView, QuestionView, QuestionDetailView, QuestionByBoardView, \
+    CommentView, CommentDetailView, CommentByBoardView, TestSubmitView, TestSubmitDetailView, TestSubmitByTestView
 
 app_name = 'classroom'
 
@@ -7,70 +11,74 @@ urlpatterns = [
     ## 클래스
     # 클래스 목록 조회 및 클래스 생성
     path('', ClassroomView.as_view()),
-    # 클래스 삭제
-    path('delete/<int:pk>/', ClassroomView.as_view()),
-    # 클래스 수정
-    path('update/<int:pk>/', ClassroomView.as_view()),
-    # 클래스 상세 조회
-    # path('detail/<int:pk>/', ClassroomDetailView.as_view()),
+    # 클래스 상세 조회, 수정, 삭제
+    path('detail/<int:pk>/', ClassroomDetailView.as_view()),
     # 클래스 태그 조회
-    # path('tag/<str:slug>/', ClassroomTagView.as_view()),
+    path('tag/', ClassroomTagView.as_view()),
     # 교사별 클래스 조회
-    # path('<str:user>/', ClassroomTeacherView.as_view()),
+    # path('<int:pk>/', ClassroomByTeacherView.as_view()),
 
     ## 문제 게시판
     # 문제 게시판 목록 조회 및 문제 게시판 생성
-    # path('test/', TestBoardView.as_view()),
-    # 문제 게시판 삭제
-    # path('test/delete/<int:pk>/', TestBoardView.as_view()),
-    # 문제 게시판 수정
-    # path('test/update/<int:pk>/', TestBoardView.as_view()),
-    # 문제 게시판 상세 조회
-    # path('test/detail/<int:pk>/', TestBoardDetailView.as_view()),
+    path('test/', TestBoardView.as_view()),
+    # 문제 게시판 상세 조회, 수정, 삭제
+    path('test/detail/<int:pk>/', TestBoardDetailView.as_view()),
     # 클래스별 문제 게시판 조회
-    # path('test/<str:classroom>/', TestBoardClassView.as_view()),
+    path('test/<int:pk>/', TestBoardByClassView.as_view()),
+
+    ## 문제 게시글
+    # 문제 게시글 목록 조회 및 문제 게시판 생성
+    path('test/post/', TestView.as_view()),
+    # 문제 게시글 상세 조회, 수정, 삭제
+    path('test/post/detail/<int:pk>/', TestDetailView.as_view()),
+    # 게시판별 문제 게시글 조회
+    path('test/post/<int:pk>/', TestByBoardView.as_view()),
 
     ## 강의자료 게시판
     # 강의자료 게시판 목록 조회 및 강의자료 게시판 생성
-    # path('lecturenote/', LectureNoteView.as_view()),
-    # 강의자료 게시판 삭제
-    # path('lecturenote/delete/<int:pk>/', LectureNoteView.as_view()),
-    # 강의자료 게시판 수정
-    # path('lecturenote/update/<int:pk>/', LectureNoteView.as_view()),
-    # 강의자료 게시판 상세 조회
-    # path('lecturenote/detail/<int:pk>/', LectureNoteDetailView.as_view()),
+    path('lecturenote/', LectureNoteBoardView.as_view()),
+    # 강의자료 게시판 상세 조회, 수정, 삭제
+    path('lecturenote/detail/<int:pk>/', LectureNoteBoardDetailView.as_view()),
     # 클래스별 강의자료 게시판 조회
-    # path('lecturenote/<str:classroom>/', LectureNoteClassView.as_view()),
+    path('lecturenote/<int:pk>/', LectureNoteBoardByClassView.as_view()),
+
+    ## 강의자료 게시글
+    # 강의자료 게시글 목록 조회 및 강의자료 게시글 생성
+    path('lecturenote/post/', LectureNoteView.as_view()),
+    # 강의자료 게시글 상세 조회, 수정, 삭제
+    path('lecturenote/post/detail/<int:pk>/', LectureNoteDetailView.as_view()),
+    # 게시판별 강의자료 게시글 조회
+    path('lecturenote/post/<int:pk>/', LectureNoteByBoardView.as_view()),
 
     ## 질문 게시판
     # 질문 게시판 목록 조회 및 질문 게시판 생성
-    # path('question/', QuestionView.as_view()),
-    # 질문 게시판 삭제
-    # path('question/delete/<int:pk>/', QuestionView.as_view()),
-    # 질문 게시판 수정
-    # path('question/update/<int:pk>/', QuestionView.as_view()),
-    # 질문 게시판 상세 조회
-    # path('question/detail/<int:pk>/', QuestionDetailView.as_view()),
+    path('question/', QuestionBoardView.as_view()),
+    # 질문 게시판 상세 조회, 수정, 삭제
+    path('question/detail/<int:pk>/', QuestionBoardDetailView.as_view()),
     # 클래스별 질문 게시판 조회
-    # path('question/<str:classroom>/', QuestionClassView.as_view()),
+    path('question/<int:pk>/', QuestionBoardByClassView.as_view()),
+
+    ## 질문 게시글
+    # 질문 게시글 목록 조회 및 질문 게시글 생성
+    path('question/', QuestionView.as_view()),
+    # 질문 게시글 상세 조회, 수정, 삭제
+    path('question/detail/<int:pk>/', QuestionDetailView.as_view()),
+    # 게시판별 질문 게시판 조회
+    path('question/<int:pk>/', QuestionByBoardView.as_view()),
 
     ## 댓글
     # 댓글 목록 조회 및 댓글 작성
-    # path('comment/', CommentView.as_view()),
-    # 댓글 삭제
-    # path('comment/delete/<int:pk>/', CommentView.as_view()),
-    # 댓글 수정
-    # path('comment/update/<int:pk>/', CommentView.as_view()),
-    # 댓글 상세 조회
-    # path('comment/detail/<int:pk>/', CommentDetailView.as_view()),
-    # 게시판별 댓글 조회
-    # path('comment/<str:board>/', CommentBoardView.as_view()),
+    path('comment/', CommentView.as_view()),
+    # 댓글 상세 조회, 삭제
+    path('comment/detail/<int:pk>/', CommentDetailView.as_view()),
+    # 게시글별 댓글 조회
+    path('comment/<int:pk>/', CommentByBoardView.as_view()),
 
     ## 문제 답변
     # 제출한 문제 답변 조회 및 문제 답변 제출
-    # path('testsubmit/', TestSubmitView.as_view()),
+    path('testsubmit/', TestSubmitView.as_view()),
     # 문제 답변 상세 조회
-    # path('testsubmit/detail/<int:pk>/', TestSubmitDetailView.as_view()),
+    path('testsubmit/detail/<int:pk>/', TestSubmitDetailView.as_view()),
     # 문제별 문제 답변 조회
-    # path('testsubmit/<str:test>/', TestSubmitTestView.as_view()),
+    path('testsubmit/<int:pk>/', TestSubmitByTestView.as_view()),
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -1,32 +1,399 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Classroom
-from .serializers import ClassroomSerializer
+from .models import Classroom, Test, TestBoard, LectureNote, LectureNoteBoard, Question, QuestionBoard, Comment, \
+    TestSubmit
+from .serializers import ClassroomSerializer, TestSerializer, TestBoardSerializer, LectureNoteSerializer, \
+    LectureNoteBoardSerializer, QuestionSerializer, QuestionBoardSerializer, CommentSerializer, \
+    TestSubmitSerializer
 
 
+### Classroom 클래스룸
 class ClassroomView(APIView):
     def get(self, request):
-        classrooms = Classroom.objects.all()
-        serializer = ClassroomSerializer(classrooms, many=True)
+        queryset = Classroom.objects.all()
+        serializer = ClassroomSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = ClassroomSerializer(data=request.data)
         if serializer.is_valid():
-            classroom = serializer.save()
+            queryset = serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        classroom = Classroom.objects.get(pk=pk)
-        classroom.delete()
-        return Response({'message': 'Classroom deleted'}, status=status.HTTP_202_ACCEPTED)
 
-    def patch(self, request, pk):
-        classroom = Classroom.objects.get(pk=pk)
-        serializer = ClassroomSerializer(classroom, data=request.data)
+class ClassroomDetailView(APIView):
+    def get(self, request, pk):
+        queryset = Classroom.objects.get(pk=pk)
+        serializer = ClassroomSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        queryset = Classroom.objects.get(pk=pk)
+        serializer = ClassroomSerializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        queryset = Classroom.objects.get(pk=pk)
+        queryset.delete()
+        return Response({'message': 'Classroom deleted'}, status=status.HTTP_202_ACCEPTED)
+
+
+class ClassroomTagView(APIView):
+    def get(self, request):
+        queryset = Classroom.objects.all()
+        tag = request.GET.get('tag', '')
+        if tag:
+            queryset = queryset.filter(tag__icontains=tag)
+
+        serializer = ClassroomSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+### /Classroom 클래스룸
+
+
+### TestBoard 문제게시판
+class TestBoardView(APIView):
+    def get(self, request):
+        queryset = TestBoard.objects.all()
+        serializer = TestBoardSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = TestBoardSerializer(data=request.data)
+        if serializer.is_valid():
+            queryset = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TestBoardDetailView(APIView):
+    def get(self, request, pk):
+        queryset = TestBoard.objects.get(pk=pk)
+        serializer = TestBoardSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        queryset = TestBoard.objects.get(pk=pk)
+        serializer = TestBoardSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        queryset = TestBoard.objects.get(pk=pk)
+        queryset.delete()
+        return Response({'message': 'TestBoard deleted'}, status=status.HTTP_202_ACCEPTED)
+
+
+class TestBoardByClassView(APIView):
+    def get(self, request, pk):
+        queryset = TestBoard.objects.filter(classroom=pk)
+        serializer = TestBoardSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+### /TestBoard 문제게시판
+
+
+### Test 문제게시글
+class TestView(APIView):
+    def get(self, request):
+        queryset = Test.objects.all()
+        serializer = TestSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = TestSerializer(data=request.data)
+        if serializer.is_valid():
+            queryset = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TestDetailView(APIView):
+    def get(self, request, pk):
+        queryset = Test.objects.get(pk=pk)
+        serializer = TestSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        queryset = Test.objects.get(pk=pk)
+        serializer = TestSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        queryset = Test.objects.get(pk=pk)
+        queryset.delete()
+        return Response({'message': 'TestBoard deleted'}, status=status.HTTP_202_ACCEPTED)
+
+
+class TestByBoardView(APIView):
+    def get(self, request, pk):
+        queryset = TestBoard.objects.filter(board=pk)
+        serializer = TestSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+### /Test 문제게시글
+
+
+### LectureNoteBoard 강의자료게시판
+class LectureNoteBoardView(APIView):
+    def get(self, request):
+        queryset = LectureNoteBoard.objects.all()
+        serializer = LectureNoteBoardSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = LectureNoteBoardSerializer(data=request.data)
+        if serializer.is_valid():
+            queryset = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LectureNoteBoardDetailView(APIView):
+    def get(self, request, pk):
+        queryset = LectureNoteBoard.objects.get(pk=pk)
+        serializer = LectureNoteBoardSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        queryset = LectureNoteBoard.objects.get(pk=pk)
+        serializer = LectureNoteBoardSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        queryset = LectureNoteBoard.objects.get(pk=pk)
+        queryset.delete()
+        return Response({'message': 'LectureNoteBoard deleted'}, status=status.HTTP_202_ACCEPTED)
+
+
+class LectureNoteBoardByClassView(APIView):
+    def get(self, request, pk):
+        queryset = LectureNoteBoard.objects.filter(classroom=pk)
+        serializer = LectureNoteBoardSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+### /LectureNoteBoard 강의자료게시판
+
+
+### LectureNote 강의자료게시글
+class LectureNoteView(APIView):
+    def get(self, request):
+        queryset = LectureNote.objects.all()
+        serializer = LectureNoteSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = LectureNoteSerializer(data=request.data)
+        if serializer.is_valid():
+            queryset = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LectureNoteDetailView(APIView):
+    def get(self, request, pk):
+        queryset = LectureNote.objects.get(pk=pk)
+        serializer = LectureNoteSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        queryset = LectureNote.objects.get(pk=pk)
+        serializer = LectureNoteSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        queryset = LectureNote.objects.get(pk=pk)
+        queryset.delete()
+        return Response({'message': 'LectureNote deleted'}, status=status.HTTP_202_ACCEPTED)
+
+
+class LectureNoteByBoardView(APIView):
+    def get(self, request, pk):
+        queryset = TestBoard.objects.filter(board=pk)
+        serializer = TestSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+### /LectureNote 강의자료게시글
+
+
+### QuestionBoard 질문게시판
+class QuestionBoardView(APIView):
+    def get(self, request):
+        queryset = QuestionBoard.objects.all()
+        serializer = QuestionBoardSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = QuestionBoardSerializer(data=request.data)
+        if serializer.is_valid():
+            queryset = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionBoardDetailView(APIView):
+    def get(self, request, pk):
+        queryset = QuestionBoard.objects.get(pk=pk)
+        serializer = QuestionBoardSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        queryset = QuestionBoard.objects.get(pk=pk)
+        serializer = QuestionBoardSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        queryset = QuestionBoard.objects.get(pk=pk)
+        queryset.delete()
+        return Response({'message': 'QuestionBoard deleted'}, status=status.HTTP_202_ACCEPTED)
+
+
+class QuestionBoardByClassView(APIView):
+    def get(self, request, pk):
+        queryset = QuestionBoard.objects.filter(classroom=pk)
+        serializer = QuestionBoardSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+### /QuestionBoard 질문게시판
+
+
+### Question 질문게시글
+class QuestionView(APIView):
+    def get(self, request):
+        queryset = Question.objects.all()
+        serializer = QuestionSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            queryset = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionDetailView(APIView):
+    def get(self, request, pk):
+        queryset = Question.objects.get(pk=pk)
+        serializer = QuestionSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        queryset = Question.objects.get(pk=pk)
+        serializer = QuestionSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        queryset = Question.objects.get(pk=pk)
+        queryset.delete()
+        return Response({'message': 'Question deleted'}, status=status.HTTP_202_ACCEPTED)
+
+
+class QuestionByBoardView(APIView):
+    def get(self, request, pk):
+        queryset = Question.objects.filter(classroom=pk)
+        serializer = QuestionSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+### /Question 질문게시글
+
+
+### Comment 댓글
+class CommentView(APIView):
+    def get(self, request):
+        queryset = Comment.objects.all()
+        serializer = CommentSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            queryset = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetailView(APIView):
+    def get(self, request, pk):
+        queryset = Comment.objects.get(pk=pk)
+        serializer = CommentSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        queryset = Comment.objects.get(pk=pk)
+        serializer = CommentSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        queryset = Comment.objects.get(pk=pk)
+        queryset.delete()
+        return Response({'message': 'Comment deleted'}, status=status.HTTP_202_ACCEPTED)
+
+
+class CommentByBoardView(APIView):
+    def get(self, request, pk):
+        queryset = Comment.objects.filter(classroom=pk)
+        serializer = CommentSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+### /Comment 댓글
+
+
+### TestSubmit 문제 답변
+class TestSubmitView(APIView):
+    def get(self, request):
+        queryset = TestSubmit.objects.all()
+        serializer = TestSubmitSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = TestSubmitSerializer(data=request.data)
+        if serializer.is_valid():
+            queryset = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TestSubmitDetailView(APIView):
+    def get(self, request, pk):
+        queryset = TestSubmit.objects.get(pk=pk)
+        serializer = TestSubmitSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        queryset = TestSubmit.objects.get(pk=pk)
+        serializer = TestSubmitSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        queryset = TestSubmit.objects.get(pk=pk)
+        queryset.delete()
+        return Response({'message': 'TestSubmit deleted'}, status=status.HTTP_202_ACCEPTED)
+
+
+class TestSubmitByTestView(APIView):
+    def get(self, request, pk):
+        queryset = TestSubmit.objects.filter(test=pk)
+        serializer = TestSubmitSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+### /TestSubmit 문제 답변
