7911d5a1921a9b2405059c5d0be8d460975f5c4b
KJH <kjh@KJHs-MacBook-Pro.local>
2023-08-28 10:52:38 +0900

    :sparkles: feat: 클래스룸 테이블 어트리뷰트 추가(updated_at)

--- classroom/models.py
+++ classroom/models.py
@@ -7,6 +7,7 @@ class Classroom(models.Model):
     class_name = models.CharField(max_length=50)
     class_info = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     tag = ArrayField(models.CharField(max_length=50, blank=True), null=True, blank=True)
 
     class Meta:
