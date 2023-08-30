6ce4cd7becdb6ce2d728506af7ef9518f07fd37d
BEOM NAMGOONG <tombeom@naver.com>
2023-08-30 16:17:28 +0900

    🚧 hotfix: PR 수정

--- user/serializers.py
+++ user/serializers.py
@@ -6,7 +6,6 @@ from allauth.account.adapter import get_adapter
 from allauth.account.utils import setup_user_email
 from user.models import User
 from user.util import nickname_generate
-from drf_spectacular.utils import extend_schema
 
 import re
 
